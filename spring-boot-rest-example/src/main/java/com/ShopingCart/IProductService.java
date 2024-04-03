51e3cff4eb06117525adffeaeadbb1ae96218c93
Akhilpillamgolla <84654724+Akhilpillamgolla@users.noreply.github.com>
2024-04-03 11:52:01 +0530

    Add files via upload

---
+++ spring-boot-rest-example/src/main/java/com/ShopingCart/IProductService.java
@@ -0,0 +1,6 @@
+package com.ShopingCart;
+import java.util.List;           // list is an interface
+public interface IProductService 
+{
+List<Product> findAll();     //find all is a method return all object 
+}
---
+++ spring-boot-rest-example/src/test/java/com/unitTesting/SpringBootRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
